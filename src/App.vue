ecc1a7e8c9b4ff086399b222093a2870ef2aefea
Alexey Slovetskih <alexslcraft@gmail.com>
2025-12-07 02:32:04 +0300

    Начало работы над фронтендом. Готов header и navbar для телефона

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,124 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <img src="/src/components/images/burger_menu.svg" alt="бургер-меню" id="burgerMenu">
+    <img src="/src/components/images/logo.svg" alt="лого" id="LogoHeader">
+    <img src="/src/components/images/profilepic.svg" alt="Профиль" id="profilePic">
   </header>
-
-  <RouterView />
+  <nav id="navPanelOne">
+    <Button class="navPanelSlidingButton">
+      <p>Язык</p>
+      <img src="/src/components/images/slidingIcon.svg" alt="слайдер" class="sliderIco">
+    </Button>
+    <input type="text" placeholder="Введите название новеллы..." id="searchNovelInput">
+    <button id="searchBtn"><img src="/src/components/images/SearchBtn.svg" alt="Поиск" ></button>
+  </nav>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+  
+<style>
+:root{
+  font-size: 62.5%;
+  --black: rgb(12, 13, 12);
+  --dark_gray: rgb(51, 51, 51);
+  --semi_dark_gray: rgb(88, 89, 89);
+  --semi_dark_gray_two: rgb(99, 99, 99);
+  --light_gray: rgb(123, 123, 123);
+  --semi_light_gray: rgb(166, 166, 166);
+  --semi_light_gray_two: rgb(217, 217, 217);
+  --white: rgb(255, 255, 255);
+  --orange: rgb(230, 120, 3);
+}
+button{
+  padding: 0;
+}
+h1{
+  margin: 0;
+  color: var(--white);
+}
+h2{
+  margin: 0;
+  color: var(--white);
+}
+h3{
+  margin: 0;
+  color: var(--white);
+}
+p{
+  margin: 0;
+  color: var(--white);
+}
+/* HEADER */
+#LogoHeader{
+  height: 4.2rem;
+}
+header{
+  min-height: 6rem;
+  background-color: var(--dark_gray);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+#burgerMenu{
+  margin-left: 2.3rem;
+  height: 1.8rem;
+}
+header>img:hover{
+  opacity: 50%;
+  cursor: pointer;
+}
+#profilePic{
+  margin-right: 1.4rem;
+  height: 4.2rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+/* MAIN */
+#navPanelOne{
+  background-color: var(--semi_dark_gray_two);
+  display: flex;
+  padding-left: 5.6rem;
+  height: 3rem;
+  align-items: center;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.navPanelSlidingButton{
+  display: flex;
+  gap: 0.2rem;
+  background-color: var(--semi_dark_gray_two);
+  border: none;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navPanelSlidingButton:hover{
+  opacity: 50%;
+  cursor: pointer;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.sliderIco{
+  width: 0.8rem;
+  height: 1.15rem;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+#searchNovelInput{
+  border: none;
+  font-size: 8px;
+  padding: 0.7rem 0.5rem 0.7rem 0.5rem;
+  background-color: var(--semi_light_gray);
+  color: white;
+  opacity: 50%;
+  width: 13.2rem;
+  line-height: 1rem;
+  outline: none;
+  margin-left: 1.55rem;
 }
-
-nav a:first-of-type {
-  border: 0;
+#searchNovelInput::placeholder{
+  color: white;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#searchBtn{
+  border: none;
+  background-color: var(--semi_dark_gray);
+  width: 2.4rem;
+  height: 2.4rem;
+}
+#searchBtn:hover{
+  opacity: 50%;
+  cursor: pointer;
 }
 </style>
